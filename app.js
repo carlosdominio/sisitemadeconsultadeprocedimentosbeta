1b2060beba2fc76b961027b510b330e6a917d478
Seu Nome Completo <chfla58@gmail.com>
2025-10-03 13:31:29 -0300

    Adição de variável de ambiente para API_URL para facilitar deploy.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = process.env.API_URL || 'http://localhost:3000/api';
 
 // Elementos DOM
 const clientSelect = document.getElementById('clientSelect');
